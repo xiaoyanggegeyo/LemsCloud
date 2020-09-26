1fa8a5b5ad943a122aac40cfca37c09e328fa917
yangshuang <1716656116@qq.com>
2020-09-26 13:38:39 +0800

    init project

--- modules/servercenter/src/main/java/com/msgcloud/ServerCenterApplication.java
+++ modules/servercenter/src/main/java/com/msgcloud/ServerCenterApplication.java
@@ -2,10 +2,12 @@ package com.msgcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class ServerCenterApplication {
+public class ServerCenterApplication{
     public static void main(String[] args) {
         SpringApplication.run(ServerCenterApplication.class);
     }
